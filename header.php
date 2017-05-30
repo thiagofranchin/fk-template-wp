7f72f90431d5b297f66dfeea24474f438a0e697e
Thiago Franchin <thiago.franchin@gmail.com>
2017-05-30 02:17:06 -0300

    secao 2 / 10

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>FK Template</title>
+    
+    
+  </head>
+  <body <?php body_class()?>>
+    <header>header
+        <div class="barra-topo">
+            <div class="reder-sociais"></div>
+            <div class="pesquisa"></div>
+        </div>
+        <div class="area-menu">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header();?>
+<div class="conteudo">
+    <main>
+        <section class="slide"></section>
+        <section class="servicos"></section>
+        <section class="meio">
+            <aside class="barra-lateral"></aside>
+            <div class="noticias"></div>
+        </section>
+        <div class="mapa"></div>
+    </main>
+</div>
+<?php get_footer();?>
